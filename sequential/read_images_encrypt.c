dfbf1299e8819fce611ba569e61fedb5885fcd05
alessandropelayo <154472296+alessandropelayo@users.noreply.github.com>
2024-10-15 12:26:47 -0700

    Read multiple folders

--- sequential/read_images_encrypt.c
+++ sequential/read_images_encrypt.c
@@ -4,42 +4,47 @@
 #include <string.h>
 
 int main() {
-    const char *folderPath = "./data/flowers/";
+    const char *folders[] = {"./data/flowers/", "./data/dogs/", "./data/cats/", "./data/horses/", "./data/human/"};
+    int numFolders = sizeof(folders) / sizeof(folders[0]);
 
 
-    struct dirent *entry;
-    DIR *dir = opendir(folderPath);
-    if (dir == NULL) {
-        printf("Could not open directory: %s\n", folderPath);
-        return -1;
-    }
-
-    while ((entry = readdir(dir)) != NULL) {
-        // Ignore the current (".") and parent ("..") directory entries
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
-            continue;
-        }
-
-        // Prepare the full path to the image file
-        char filePath[256];
-        snprintf(filePath, sizeof(filePath), "%s%s", folderPath, entry->d_name);
-
-        // Prepare the command to call the rsa_encrypter with the file path
-        char command[300];
-        snprintf(command, sizeof(command), "./rsa_image_encrypter %s", filePath);
-
-        // Call the program
-        int result = system(command);
-
-        // Check for errors in running the command
-        if (result == -1) {
-            printf("Error executing image loader for file: %s\n", filePath);
-            closedir(dir);
+    for (int i = 0; i < numFolders; i++) {
+        const char *folderPath = folders[i];
+        struct dirent *entry;
+        DIR *dir = opendir(folderPath);
+        if (dir == NULL) {
+            printf("Could not open directory: %s\n", folderPath);
             return -1;
         }
 
-        printf("Processed file: %s\n", filePath);
+        while ((entry = readdir(dir)) != NULL) {
+            // Ignore the current (".") and parent ("..") directory entries
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+                continue;
+            }
+
+            // Prepare the full path to the image file
+            char filePath[256];
+            snprintf(filePath, sizeof(filePath), "%s%s", folderPath, entry->d_name);
+
+            // Prepare the command to call the rsa_encrypter with the file path
+            char command[300];
+            snprintf(command, sizeof(command), "./rsa_image_encrypter %s", filePath);
+
+            // Call the program
+            int result = system(command);
+
+            // Check for errors in running the command
+            if (result == -1) {
+                printf("Error executing image loader for file: %s\n", filePath);
+                closedir(dir);
+                return -1;
+            }
+
+            printf("Processed file: %s\n", filePath);
+        }
+        closedir(dir);
     }
-    closedir(dir);
+    
     return 0;
 }
